c13c8c5f908e01ecfbf6fbc7b7535086b8fe05cb
Hossein Abedi <hosein.abedi2000@gmail.com>
2023-11-06 23:58:32 +0330

    Header added

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { FaSearch } from "react-icons/fa";
+
+const Header = () => {
+    return(
+        <header className="bg-slate-200 shadow-md">
+            <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
+                <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
+                    <span className="text-slate-500">Nest</span>
+                    <span className="text-slate-700">Finder</span>
+                </h1>
+                <form className="bg-slate-100 p-3 rounded-lg flex items-center">
+                    <input type="text" placeholder="Search..." className="bg-transparent focus:outline-none" />
+                    <FaSearch className="text-slate-600" />
+                </form>
+            </div>
+        </header>
+    )
+};
+
+export default Header;
