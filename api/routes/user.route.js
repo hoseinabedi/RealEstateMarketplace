3243c93d3c40e9e1f34b83bde43b2685574c8b2d
Hossein Abedi <hosein.abedi2000@gmail.com>
2023-11-10 23:51:29 +0330

    test route added and controller defined

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,11 +1,8 @@
 import express from 'express';
+import { test } from '../controllers/user.controller.js';
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.json({
-        message: 'Hello World!'
-    }, 200);
-});
+router.get('/test', test);
 
 export default router;
