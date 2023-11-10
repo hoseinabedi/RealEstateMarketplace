aeed3303092503b5d4ee0be6db91a6e74151da1c
Hossein Abedi <hosein.abedi2000@gmail.com>
2023-11-10 23:15:32 +0330

    Routes added

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRouter from './routes/user.route.js';
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(() => {
@@ -12,9 +13,7 @@ mongoose.connect(process.env.MONGO).then(() => {
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
+app.use("/api/user", userRouter);
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}!`);
