b27a4f54a58d5b147ab4ec0c0f66fb32130007e6
Hossein Abedi <hosein.abedi2000@gmail.com>
2023-11-10 23:01:55 +0330

    User model added

--- api/index.js
+++ api/index.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(() => {
     console.log('Connected to MongoDB');
-}).catch ((err) => {
+}).catch((err) => {
     console.log(err);
 });
 
