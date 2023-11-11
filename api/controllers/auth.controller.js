ea8c718d09c4ba67b48da9b5c212477254ba0b6f
Hossein Abedi <hosein.abedi2000@gmail.com>
2023-11-12 00:02:47 +0330

    User controller fixed - Api test fixed

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,3 +1,8 @@
-export const signup = (req, res) => {
-    console.log(req.body);
+import User from "../models/user.model.js";
+
+export const signup = async(req, res) => {
+    const { username, name, email, password } = req.body;
+    const user = new User({ username, name, email, password });
+    await user.save();
+    res.status(201).json({ message: "User created successfully" });
 };
