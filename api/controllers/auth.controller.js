8c25e71de1171dc3133e252eaf2777ef1972a9ce
Hossein Abedi <hosein.abedi2000@gmail.com>
2023-11-20 23:29:11 +0330

    add signup api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -5,6 +5,10 @@ export const signup = async(req, res) => {
     const { username, name, email, password } = req.body;
     const hashedPassword = bcryptjs.hashSync(password, 10);
     const user = new User({ username, name, email, password: hashedPassword });
-    await user.save();
-    res.status(201).json({ message: "User created successfully" });
+    try{
+        await user.save();
+        res.status(201).json({ message: "User created successfully" });
+    }catch(err){
+        res.status(400).json({ message: err.message });
+    }
 };
