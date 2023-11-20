4e88e491b46c07e7c32a9826b50ead064b93f0d8
Hossein Abedi <hosein.abedi2000@gmail.com>
2023-11-20 22:45:47 +0330

    Store hash passwords in DataBase

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,8 +1,10 @@
 import User from "../models/user.model.js";
+import bcryptjs from "bcryptjs";
 
 export const signup = async(req, res) => {
     const { username, name, email, password } = req.body;
-    const user = new User({ username, name, email, password });
+    const hashedPassword = bcryptjs.hashSync(password, 10);
+    const user = new User({ username, name, email, password: hashedPassword });
     await user.save();
     res.status(201).json({ message: "User created successfully" });
 };
